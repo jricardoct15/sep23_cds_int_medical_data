e65022c11c86ae1ad6bdc03ec4759ed365026f25
jricardoct15 <65625800+jricardoct15@users.noreply.github.com>
2024-06-27 12:28:52 +0200

    Add files via upload

--- Streamlit_medical_data.py
+++ Streamlit_medical_data.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Lung X-Ray : classification project")
+
+st.write("### Load an X-Ray JPG image to classify")
+
 uploaded_file = st.file_uploader("Choose a file", type='jpg')
 if uploaded_file is not None:
     # To read file as bytes:

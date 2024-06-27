98433eee710284ff85a1b109a8759762b3b67098
jricardoct15 <65625800+jricardoct15@users.noreply.github.com>
2024-06-27 14:56:22 +0200

    Add files via upload

--- Streamlit_medical_data.py
+++ Streamlit_medical_data.py
@@ -28,5 +28,5 @@ if uploaded_file is not None:
     file = uploaded_file.read() # Read the data
     st.write("The image ", uploaded_file.name, "was load successufully, with ",uploaded_file.size," Bytes.")
     st.image(uploaded_file)
-    PlotXRay(cv2.imread(uploaded_file,format='jpg'))
+    PlotXRay(cv2.imread(uploaded_file))
     

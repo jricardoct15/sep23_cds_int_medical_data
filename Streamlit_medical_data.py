9176c99d3feea6ffaf04f217912f04cef483d588
jricardoct15 <65625800+jricardoct15@users.noreply.github.com>
2024-06-27 12:37:36 +0200

    Add files via upload

--- Streamlit_medical_data.py
+++ Streamlit_medical_data.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+
+#Plot grayscale Image function
+def PlotXRay (Image):
+  plt.figure(figsize = (8,5))
+  if len(Image.shape) == 2 :
+    plt.imshow(Image, cmap = 'gray')
+  else:
+    plt.imshow(Image, cmap = 'RGB')
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.show();
 
 st.title("Lung X-Ray : classification project")
 
@@ -9,5 +22,7 @@ uploaded_file = st.file_uploader("Choose a file", type='jpg')
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
+    #st.write(bytes_data)
+    file = uploaded_file.read() # Read the data
+    image_result = open(uploaded_file.name, 'wb')
+    PlotXRay (image_result)

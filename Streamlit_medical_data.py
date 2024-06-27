310d4b395751bd8e32c9d5c7fbe4d06f115b9a38
jricardoct15 <65625800+jricardoct15@users.noreply.github.com>
2024-06-27 12:23:16 +0200

    Add files via upload

--- Streamlit_medical_data.py
+++ Streamlit_medical_data.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file", type='jpg')
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()

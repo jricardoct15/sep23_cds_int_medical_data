df31aabfa03c95e047f0ccb1c0bd07b6cdaba033
jricardoct15 <65625800+jricardoct15@users.noreply.github.com>
2024-06-28 11:10:18 +0200

    Add files via upload

--- Streamlit_medical_data.py
+++ Streamlit_medical_data.py
@@ -23,20 +23,20 @@ st.write("### Load an X-Ray JPG image to classify")
 
 uploaded_file = st.file_uploader("Choose a file", type='jpg')
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
+  # To read file as bytes:
+  bytes_data = uploaded_file.getvalue()
     
-    file = uploaded_file.read() # Read the data
-    st.write("The image ", uploaded_file.name, "was load successufully, with ",uploaded_file.size," Bytes.")
-    st.image(uploaded_file)
-    PlotXRay(cv2.imread(uploaded_file))
+  file = uploaded_file.read() # Read the data
+  st.write("The image ", uploaded_file.name, "was load successufully, with ",uploaded_file.size," Bytes.")
+  st.image(uploaded_file)
+  #PlotXRay(cv2.imread(uploaded_file))
     
-    #Load the model
-    model = tf.keras.models.load_model('Ricardo Model2.h5')
-    st.write(model.summary())
+  #Load the model
+  model = tf.keras.models.load_model('Ricardo Model2.h5')
+  st.write(model.summary())
 
-    #predict
-    st.write('### Prediction')
-    predictonClass = model.predict(uploaded_file)
-    st.write("The prediction for the image ", uploaded_file.name, "is ", predictonClass)
+  #predict
+  st.write('### Prediction')
+  predictonClass = model.predict(uploaded_file)
+  st.write("The prediction for the image ", uploaded_file.name, "is ", predictonClass)
 
